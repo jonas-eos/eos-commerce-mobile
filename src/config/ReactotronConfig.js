a505d9b7b62d9de96aa66cfe9121af4356b6aa62
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-02 20:48:56 -0300

    chore: reactotron setting

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,13 @@
+import Reactotron from 'reactotron-react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+    .configure()
+    .useReactNative()
+    .use(reactotronRedux())
+    .connect();
+
+  console.tron = tron; /* eslint-disable-line no-console */
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 
 import store from './store';
