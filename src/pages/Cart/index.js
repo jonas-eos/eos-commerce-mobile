7773a9ff5e83c8b8e39b2ce2412d0ba857c9ec92
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-02 21:49:14 -0300

    chore: arraylist on cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { PropTypes } from 'prop-types';
 
-// import { Container } from './styles';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import colors from '../../styles/global';
 
@@ -25,41 +26,38 @@ import {
   OrderText,
 } from './styles';
 
-export default function CartScreen() {
+function CartScreen({ cart }) {
   return (
     <Container>
       <Products>
-        <Product>
-          <ProductInformation>
-            <ProductImage
-              source={{
-                uri:
-                  'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1529520060550',
-              }}
-            />
-            <ProductDetails>
-              <ProductName>Amount</ProductName>
-              <ProductPrice>$ 11.000,00</ProductPrice>
-            </ProductDetails>
-            <ProductDelete>
-              <Icon name="delete-forever" size={24} color={colors.main} />
-            </ProductDelete>
-          </ProductInformation>
-          <ProductControls>
-            <ProductControlButton>
-              <Icon
-                name="remove-circle-outline"
-                size={20}
-                color={colors.main}
-              />
-            </ProductControlButton>
-            <ProductAmount value={String(3)} />
-            <ProductControlButton>
-              <Icon name="add-circle-outline" size={20} color={colors.main} />
-            </ProductControlButton>
-            <ProductTotalPrice>R$ 33.000,00</ProductTotalPrice>
-          </ProductControls>
-        </Product>
+        {cart.map(product => (
+          <Product key={product.id}>
+            <ProductInformation>
+              <ProductImage source={{ uri: product.image }} />
+              <ProductDetails>
+                <ProductName>Amount</ProductName>
+                <ProductPrice>{product.priceFormatted}</ProductPrice>
+              </ProductDetails>
+              <ProductDelete>
+                <Icon name="delete-forever" size={24} color={colors.main} />
+              </ProductDelete>
+            </ProductInformation>
+            <ProductControls>
+              <ProductControlButton>
+                <Icon
+                  name="remove-circle-outline"
+                  size={20}
+                  color={colors.main}
+                />
+              </ProductControlButton>
+              <ProductAmount value={String(3)} />
+              <ProductControlButton>
+                <Icon name="add-circle-outline" size={20} color={colors.main} />
+              </ProductControlButton>
+              <ProductTotalPrice>R$ 33.000,00</ProductTotalPrice>
+            </ProductControls>
+          </Product>
+        ))}
       </Products>
       <OrderContainer>
         <TotalTitle>Total</TotalTitle>
@@ -71,3 +69,19 @@ export default function CartScreen() {
     </Container>
   );
 }
+
+CartScreen.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      image: PropTypes.any,
+      priceFormatted: PropTypes.string,
+    })
+  ).isRequired,
+};
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(CartScreen);
