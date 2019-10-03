8fdd98cbf43f5928c2155054fbdde072b9a69fcd
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-02 22:26:31 -0300

    chore: handle item amount home to cart

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -3,17 +3,33 @@
  * This document update global state
  * in app.
  */
+import produce from 'immer';
+
 export default function cart(state = [], action) {
   switch (action.type) {
-    /** action to add itens to cart */
+    /**
+     * Action to add itens to cart
+     * Before changing the global state, it is checked if there are already any
+     * items in the cart.
+     * If any current item with same id from the action item exists, the item
+     * quantity is manipulated, preventing the item from appearing duplicate in
+     * the cart, so every time you add a new item through HomeScreen, add +1 to
+     * the item quantity.
+     */
     case 'ADD_TO_CART':
-      return [
-        ...state,
-        {
-          ...action.item,
-          amount: 1,
-        },
-      ];
+      return produce(state, draft => {
+        const nextState = draft;
+
+        const productIndex = nextState.findIndex(
+          product => product.id === action.item.id
+        );
+
+        if (productIndex >= 0) {
+          nextState[productIndex].amount += 1;
+        } else {
+          draft.push({ ...action.item, amount: 1 });
+        }
+      });
     default:
       return state;
   }
