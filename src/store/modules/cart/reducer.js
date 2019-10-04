a3d636bc01b4873c9a85e03a5d825b8799f0f304
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-03 23:37:45 -0300

    chore: transfered business rules in reducer to sagas

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -7,28 +7,11 @@ import produce from 'immer';
 
 export default function cart(state = [], action) {
   switch (action.type) {
-    /**
-     * Action to add itens to cart
-     * Before changing the global state, it is checked if there are already any
-     * products in the cart.
-     * If any current product with same id from the action product exists, the product
-     * quantity is manipulated, preventing the product from appearing duplicate in
-     * the cart, so every time you add a new product through HomeScreen, add +1 to
-     * the product quantity.
-     */
+    /** Action to add itens to cart */
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const nextState = draft;
-
-        const productIndex = nextState.findIndex(
-          product => product.id === action.product.id
-        );
-
-        if (productIndex >= 0) {
-          nextState[productIndex].amount += 1;
-        } else {
-          draft.push({ ...action.product, amount: 1 });
-        }
+        const { product } = action;
+        draft.push(product);
       });
 
     /**
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,20 +1,39 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { formatUsd } from '../../../util/format';
+import { addToCartSuccess, updateAmount } from './actions';
 
 /**
- * This method get a product by id via API REST
- * Call the add to cart action, passing all product data to update int the
- * global state.
+ * Searches the global state for a product according to the request id.
+ * If the product is already in the state, only add mount in cart.
+ * If the product is not in state, a request is sent to the API, and the cart
+ * is updated with the return data from the API.
  * @param {Number} productId
  * @generator
  * @yields {object} The product data.
  */
 function* addToCart({ productId }) {
-  const response = yield call(api.get, `/products/${productId}`);
+  /** Looking foward product by id */
+  const productExists = yield select(state =>
+    state.cart.find(product => product.id === productId)
+  );
 
-  yield put(addToCartSuccess(response.data));
+  /** Rules to manipulate the cart */
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield put(updateAmount(productId, amount));
+  } else {
+    const response = yield call(api.get, `/products/${productId}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatUsd(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
