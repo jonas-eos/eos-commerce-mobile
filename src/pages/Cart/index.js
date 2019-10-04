5cb88f2b3c425781b454a26681087b41f8c8162e
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-03 21:34:11 -0300

    chore: removed unuseless code

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { PropTypes } from 'prop-types';
-import { FlatList } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import colors from '../../styles/global';
