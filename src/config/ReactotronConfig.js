0c4b4606bb1cabdfc976498f1b39aa6678cd4eb9
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-03 23:15:04 -0300

    chore: fix reactotronConfig

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,14 @@
 import Reactotron from 'reactotron-react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
     .configure()
     .useReactNative()
+    .use(reactotronSaga())
+    .use(reactotronRedux())
     .connect();
 
   console.tron = tron;
