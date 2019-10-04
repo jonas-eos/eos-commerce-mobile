2f6fa4d2a77747b0f59ddd1275720308db29fd0b
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-04 02:29:25 -0300

    chore: android build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { StatusBar } from 'react-native';
+import 'intl';
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/pt-BR';
 
 import './config/ReactotronConfig';
 
--- src/util/format.js
+++ src/util/format.js
@@ -1,11 +1,11 @@
 /** Format currency to standard us type, using USD as currency. */
-export const { format: formatUsd } = new Intl.NumberFormat('us', {
+export const { format: formatUsd } = new Intl.NumberFormat(undefined, {
   style: 'currency',
   currency: 'USD',
 });
 
 /** Format currency to standard us type, using BRL as currency. */
-export const { format: formatBrl } = new Intl.NumberFormat('pt-BR', {
+export const { format: formatBrl } = new Intl.NumberFormat(undefined, {
   style: 'currency',
   currency: 'BRL',
 });
